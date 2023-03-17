735656937f175629a512098aede114993f0a6da5
sookwalinga <sookwalinga@gmail.com>
2023-03-17 16:15:04 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, k, l;
+
+    for (i = 0; i < 100; i++)
+    {
+        for (j = i + 1; j < 100; j++)
+        {
+            k = i / 10;
+            l = i % 10;
+            putchar(k + '0');
+            putchar(l + '0');
+            putchar(' ');
+            k = j / 10;
+            l = j % 10;
+            putchar(k + '0');
+            putchar(l + '0');
+            if (i != 98 || j != 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
+
+    return 0;
+}
