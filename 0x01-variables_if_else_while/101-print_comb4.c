50f8545e40360ce2e7678bdb74739ef03b9ecafb
Samuel Okwalinga <70289309+sookwalinga@users.noreply.github.com>
2023-03-18 02:18:45 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,28 +7,28 @@
  */
 int main(void)
 {
-    int i, j, k;
+int i, j, k;
 
-    for (i = 0; i <= 7; i++)
-    {
-        for (j = i + 1; j <= 8; j++)
-        {
-            for (k = j + 1; k <= 9; k++)
-            {
-                putchar(i + '0');
-                putchar(j + '0');
-                putchar(k + '0');
+for (i = 0; i <= 7; i++)
+{
+for (j = i + 1; j <= 8; j++)
+{
+for (k = j + 1; k <= 9; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
 
-                if (i == 7 && j == 8 && k == 9)
-                    break;
+if (i == 7 && j == 8 && k == 9)
+break;
 
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+putchar(',');
+putchar(' ');
+}
+}
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }
