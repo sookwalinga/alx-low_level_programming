22e731bdcc3037cecbe0cd407702224d542989c0
Samuel Okwalinga <70289309+sookwalinga@users.noreply.github.com>
2023-03-18 02:17:25 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    int i, j, k, l;
+int i, j, k, l;
 
-    for (i = 0; i < 100; i++)
-    {
-        for (j = i + 1; j < 100; j++)
-        {
-            k = i / 10;
-            l = i % 10;
-            putchar(k + '0');
-            putchar(l + '0');
-            putchar(' ');
-            k = j / 10;
-            l = j % 10;
-            putchar(k + '0');
-            putchar(l + '0');
-            if (i != 98 || j != 99)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
+for (i = 0; i < 100; i++)
+{
+for (j = i + 1; j < 100; j++)
+{
+k = i / 10;
+l = i % 10;
+putchar(k + '0');
+putchar(l + '0');
+putchar(' ');
+k = j / 10;
+l = j % 10;
+putchar(k + '0');
+putchar(l + '0');
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
 
-    return 0;
+return (0);
 }
