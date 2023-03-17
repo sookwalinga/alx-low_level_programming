90af4d2b95d4a207fb72d8072a763db5c33750fe
sookwalinga <sookwalinga@gmail.com>
2023-03-16 18:18:59 -0700

    add doc

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,13 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a quote to the standard error.
+ *
+ * Return: Always 1 to indicate an error occurred.
+ */
+
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
