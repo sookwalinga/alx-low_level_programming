87cd2ba54534992176f945bb6d154df32d3a6973
Samuel Okwalinga <70289309+sookwalinga@users.noreply.github.com>
2023-03-18 02:19:53 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-    int i, j, k;
+int i, j, k;
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = i + 1; j < 10; j++)
-        {
-            k = 10 * i + j;
+for (i = 0; i < 8; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+k = 10 * i + j;
 
-            putchar((k / 10) + '0');
-            putchar((k % 10) + '0');
+putchar((k / 10) + '0');
+putchar((k % 10) + '0');
 
-            if (i < 8)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
+if (i < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
 
-    return (0);
+return (0);
 }
