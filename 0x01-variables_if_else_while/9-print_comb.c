de3e87ab54566548a4242db6f09e551605c09a1c
Samuel Okwalinga <70289309+sookwalinga@users.noreply.github.com>
2023-03-18 02:08:23 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,18 @@
  */
 int main(void)
 {
-    int i;
+int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + '0');
-        if (i != 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
 
-    putchar('\n');
-
-    return (0);
+return (0);
 }
