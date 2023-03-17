23ca796defb6bf218d8c94204396d64b794c2140
Samuel Okwalinga <70289309+sookwalinga@users.noreply.github.com>
2023-03-18 01:54:04 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char c;
+char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		if (c != 'e' && c != 'q')
-			_putchar(c);
-	}
-	_putchar('\n');
-	return (0);
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'e' && c != 'q')
+_putchar(c);
+}
+_putchar('\n');
+return (0);
 }
