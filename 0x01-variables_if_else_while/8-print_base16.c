1ced7f329e6a960ee0a177d4c7fe8dc31799a93c
sookwalinga <sookwalinga@gmail.com>
2023-03-17 16:02:41 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    for (i = 0; i < 10; i++)
+        putchar(i + '0');
+    for (i = 'a'; i <= 'f'; i++)
+        putchar(i);
+    putchar('\n');
+    return 0;
+}
