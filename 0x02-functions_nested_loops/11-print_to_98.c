bc7fd526b21d429c634ccb0ec0f19841b821e6fe
sookwalinga <sookwalinga@gmail.com>
2023-03-21 16:21:58 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+int i;
+
+if (n <= 98)
+{
+for (i = n; i < 98; i++)
+printf("%d, ", i);
+}
+else
+{
+for (i = n; i > 98; i--)
+printf("%d, ", i);
+}
+
+printf("98\n");
+}
