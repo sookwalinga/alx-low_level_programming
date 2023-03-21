348e8757d94887f17eeaa9fd970b3f06c9dea61b
Samuel Okwalinga <70289309+sookwalinga@users.noreply.github.com>
2023-03-22 02:38:50 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,11 +14,14 @@ if (n < 0)
 n = -n;
 
 /* get the last digit */
+if (n == 0)
+last_digit = 0;
+else
 last_digit = n % 10;
 
 /* print the last digit */
 _putchar('0' + last_digit);
 
 /* return the last digit */
-return last_digit;
+return (last_digit);
 }
