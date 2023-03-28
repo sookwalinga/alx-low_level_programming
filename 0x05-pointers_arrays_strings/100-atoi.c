eeacf559dac0ac4cf6ee745cee6be66912956c45
Samuel Okwalinga <70289309+sookwalinga@users.noreply.github.com>
2023-03-28 05:09:28 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,4 @@
+
 /**
  * _atoi - Converts a string to an integer.
  * @s: The string to convert.
@@ -15,12 +16,7 @@ if (s[i] == '-') {
 sign = sign * -1;
 }
 if (s[i] >= '0' && s[i] <= '9') {
-int digit = s[i] - '0';
-if (num > INT_MAX / 10 || (num == INT_MAX / 10 && digit > INT_MAX % 10)) {
-/* Integer overflow */
-return sign == 1 ? INT_MAX : INT_MIN;
-}
-num = num * 10 + digit;
+num = num * 10 + (s[i] - '0');
 if (s[i + 1] < '0' || s[i + 1] > '9') {
 break;
 }
