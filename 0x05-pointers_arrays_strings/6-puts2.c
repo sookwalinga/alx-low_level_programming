0f122af13a4e7fa95ead7b67ec9b1666cfd1c97c
sookwalinga <sookwalinga@gmail.com>
2023-03-27 18:38:25 -0700

    Create emacs 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * starting with the first character, followed by a new line
+ *
+ * @str: Pointer to the first character of the string
+ */
+void puts2(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i += 2)
+{
+_putchar(str[i]);
+}
+
+_putchar('\n');
+}
