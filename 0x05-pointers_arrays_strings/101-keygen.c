633dcdfe495b177af9f67fea8b9923d820f76a83
sookwalinga <sookwalinga@gmail.com>
2023-03-27 19:02:06 -0700

    Create 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+int main(void)
+{
+char password[PASSWORD_LENGTH + 1] = {'\0'};
+const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+srand(time(NULL));
+
+for (int i = 0; i < PASSWORD_LENGTH; i++)
+{
+password[i] = charset[rand() % (sizeof(charset) - 1)];
+}
+
+printf("Generated password: %s\n", password);
+
+ return (0);
+}
