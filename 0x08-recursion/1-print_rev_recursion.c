ed41a859bb8ed16add834918dad51cf8448f561e
Samuel Okwalinga <70289309+sookwalinga@users.noreply.github.com>
2023-04-06 16:18:41 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _print_rev_recursion - Prints a string in reverse recursively
- * @s: The string to be printed
+ * _print_rev_recursion - Function that prints a string in reverse
+ * @s: Pointer to string
  *
- * Return: void
+ * Return: string
  */
 void _print_rev_recursion(char *s)
 {
-if (*s)
+if (*s == '\0')
 {
-_print_rev_recursion(s + 1);
-_putchar(*s);
+return;
 }
+_print_rev_recursion(s + 1);
+putchar(*s);
 }
