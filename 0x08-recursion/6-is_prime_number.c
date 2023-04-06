fd711ced7db8bc5be1a0fb7b3ce31b9f3b2f536b
Samuel Okwalinga <70289309+sookwalinga@users.noreply.github.com>
2023-04-06 16:17:34 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
 /**
- * is_prime_number - checks whether a number is prime or not
+ * is_prime_number - Function that returns 1 if the
+ *                   input integer is a prime number,
+ *                   otherwise return 0
+ * @n: Number to get be checked
  *
- * @n: the number to check
- *
- * Return: 1 if the number is prime, 0 otherwise
+ * Return: 1 if n is prime number and 0 if it is not
  */
 int is_prime_number(int n)
 {
-/* base case */
-if (n <= 1)
-return (0);
 
-return (check_prime(n, 2)); /* start checking from 2 */
-}
-
-/**
- * check_prime - helper function to check if a number is prime or not
- *
- * @n: the number to check
- * @i: the number to check for divisibility
- *
- * Return: 1 if the number is prime, 0 otherwise
- */
-int check_prime(int n, int i)
+if (n == 2)
 {
-/* base case */
-if (n % i == 0)
-return (0);
-
-if (i >= n / 2) /* we only need to check up to n/2 */
 return (1);
-
-return (check_prime(n, i + 1)); /* check next number */
+}
+else if (n > 1 && n % 2 == 1)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
