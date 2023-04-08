08da76f76bc22278b2f85436dd14a5eb87e5aa5f
Samuel Okwalinga <70289309+sookwalinga@users.noreply.github.com>
2023-04-08 23:11:51 +0300

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,27 +1,20 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
- * _strchr - locates a character in a string
- *
- * @s: pointer to the string to search
- * @c: character to find in the string
- *
- * Return: pointer to the first occurrence of the character c in the string s,
- * or NULL if the character is not found
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
+int i = 0;
+
+for (; s[i] >= '\0'; i++)
 {
-if (*s == c)
-{
-return (s);
+if (s[i] == c)
+return (&s[i]);
 }
-s++;
-}
-if (c == '\0')
-{
-return (s);
-}
-return (NULL);
+return (0);
 }
