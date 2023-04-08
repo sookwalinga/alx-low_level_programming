3fca7413640747bb9abfbf665699ea6a2b8023dc
sookwalinga <sookwalinga@gmail.com>
2023-04-08 12:48:05 -0700

    2-strlen

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: string to get the length of
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+int len = 0;
+
+while (*s != '\0')
+{
+len++;
+s++;
+}
+
+return (len);
+}
