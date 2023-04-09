4d38d03aeda492300ac3437209f13220f606d442
Samuel Okwalinga <70289309+sookwalinga@users.noreply.github.com>
2023-04-10 00:56:22 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-printf("%s\n", argv[0]);
+printf("%s\n", *argv);
 return (0);
 }
