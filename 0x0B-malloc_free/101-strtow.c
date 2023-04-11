1ef9e2637d7ac12017583dc3db22c2facef7370c
Samuel Okwalinga <70289309+sookwalinga@users.noreply.github.com>
2023-04-12 00:18:25 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,76 +1,113 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-/**
- * count_words - Counts the number of words in a string
- * @str: The input string
- *
- * Return: The number of words
- */
-int count_words(char *str)
-{
-int count = 0, i;
-
-for (i = 0; str[i] != '\0'; ++i)
-{
-if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-{
-++count;
-}
-}
-return (count);
-}
+int count_words(char *str);
+int find_word_length(char *str);
+char **create_array(int size);
 
 /**
  * strtow - Splits a string into words
- * @str: The input string
+ * @str: The string to split
  *
  * Return: A pointer to an array of strings (words)
  */
 char **strtow(char *str)
 {
-char **words;
-int i, j, k, len, count;
+int i, j, k, len, words;
+char **arr;
 
 if (str == NULL || *str == '\0')
+return (NULL);
+
+words = count_words(str);
+if (words == 0)
+return (NULL);
+
+arr = create_array(words + 1);
+if (arr == NULL)
+return (NULL);
+
+i = 0;
+while (i < words)
 {
+while (*str == ' ')
+str++;
+
+len = find_word_length(str);
+
+arr[i] = malloc((len + 1) * sizeof(char));
+if (arr[i] == NULL)
+{
+while (i >= 0)
+free(arr[i--]);
+free(arr);
 return (NULL);
 }
-count = count_words(str);
-words = malloc((count + 1) * sizeof(char *));
-if (words == NULL)
+
+for (j = 0, k = 0; j < len; j++, k++)
 {
+arr[i][k] = *(str++);
+if (*(str) == ' ' || *(str) == '\0')
+break;
+}
+
+arr[i][k] = '\0';
+i++;
+}
+
+arr[i] = NULL;
+return (arr);
+}
+
+/**
+ * count_words - Counts the number of words in a string
+ * @str: The string to count the words of
+ *
+ * Return: The number of words in the string
+ */
+int count_words(char *str)
+{
+int i, count;
+
+for (i = 0, count = 0; str[i] != '\0'; i++)
+{
+if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+count++;
+}
+
+return (count);
+}
+
+/**
+ * find_word_length - Finds the length of a word in a string
+ * @str: The string to find the word in
+ *
+ * Return: The length of the word
+ */
+int find_word_length(char *str)
+{
+int len;
+
+for (len = 0; str[len] != ' ' && str[len] != '\0'; len++)
+;
+
+return (len);
+}
+
+/**
+ * create_array - Creates an array of char pointers
+ * @size: The size of the array to create
+ *
+ * Return: A pointer to the array
+ */
+char **create_array(int size)
+{
+char **arr;
+
+arr = malloc(size * sizeof(char *));
+if (arr == NULL)
 return (NULL);
-}
-for (i = 0, j = 0; i < count; ++i)
-{
-while (str[j] == ' ')
-{
-++j;
-}
-len = 0;
-while (str[j + len] != ' ' && str[j + len] != '\0')
-{
-++len;
-}
-words[i] = malloc((len + 1) * sizeof(char));
-if (words[i] == NULL)
-{
-for (k = 0; k < i; ++k)
-{
-free(words[k]);
-}
-free(words);
-return (NULL);
-}
-for (k = 0; k < len; ++k)
-{
-words[i][k] = str[j + k];
-}
-words[i][k] = '\0';
-j += len;
-}
-words[i] = NULL;
-return (words);
+
+return (arr);
 }
