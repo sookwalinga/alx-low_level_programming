67f9c56b679ebe789ae4b940a2772471a6794426
sookwalinga <sookwalinga@gmail.com>
2023-04-11 14:09:14 -0700

    Create 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,8 @@
+/**
+ * alloc_grid - returns a pointer to a 2D array of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: pointer to the 2D array, NULL on failure
+ */
+int **alloc_grid(int width, int height);
