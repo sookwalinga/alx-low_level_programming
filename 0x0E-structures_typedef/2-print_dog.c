21d47352c916f058d705dee4e4cff36d8fb5cec0
sam <sookwalinga@gmail.com>
2023-04-18 00:44:13 +0300

    feat: create 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints the information of a dog
+ * @d: pointer to a dog struct
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+return;
+
+printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+printf("Age: %.6f\n", d->age);
+printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+}
