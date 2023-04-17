da5e509d5fc8981307484298ccb2200838b73749
sam <sookwalinga@gmail.com>
2023-04-18 00:34:41 +0300

    fix: betty is still not ok

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,12 @@
 #define DOG_H
 
 /**
-
  * struct dog - a dog's basic info
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the dog's owner
  *  
- * Description: A structure that holds information about a dog
+ * Description: holds information about a dog
 */
 
 struct dog
@@ -27,5 +26,7 @@ void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif /* DOG_H */
