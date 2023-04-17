b8f26c28245aa558a3cf7148decd20875dcd705c
sam <sookwalinga@gmail.com>
2023-04-18 00:27:55 +0300

    refactor: dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,23 @@
-struct dog {
-    char *name;
-    float age;
-    char *owner;
-};
+#ifndef DOG_H
+#define DOG_H
+
+/**
+
+struct dog - a dog's basic info
+@name: name of the dog
+@age: age of the dog
+@owner: name of the dog's owner
+Description: A structure that holds information about a dog
+*/
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
