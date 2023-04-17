462471c150cc986b2a99036512f3bd1822ab9efd
sam <sookwalinga@gmail.com>
2023-04-18 00:35:43 +0300

    fix: ok last try

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,8 +22,8 @@ char *owner;
  */
 typedef struct dog dog_t;
 
-void init_dog(dog_t *d, char *name, float age, char *owner);
-void print_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
