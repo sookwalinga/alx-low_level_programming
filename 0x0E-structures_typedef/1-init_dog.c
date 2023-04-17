e5395fd82d32b2fa49cd0846a2d48ec45891c45e
sam <sookwalinga@gmail.com>
2023-04-18 00:39:49 +0300

    feat: create 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+/**
+ * init_dog - initializes a struct dog
+ * @d: pointer to the struct dog to initialize
+ * @name: name to set
+ * @age: age to set
+ * @owner: owner to set
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
