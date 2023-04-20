fdbb7a56268b042835cdb601328fb9b7c11ba495
sam <sookwalinga@gmail.com>
2023-04-20 23:46:15 +0300

    refactor

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,33 +1,46 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_strings - prints strings, followed by a new line
- * @separator: the string to be printed between the strings
- * @n: the number of strings passed to the function
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
-void print_strings(const char *separator, const unsigned int n, ...)
+void print_all(const char * const format, ...)
 {
 va_list args;
-unsigned int i;
-char *str;
+char *str, *sep = "";
 
-va_start(args, n);
+va_start(args, format);
 
-for (i = 0; i < n; i++)
+if (format != NULL)
 {
+for (unsigned int i = 0; format[i] != '\0'; i++)
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", sep, va_arg(args, int));
+break;
+case 'i':
+printf("%s%d", sep, va_arg(args, int));
+break;
+case 'f':
+printf("%s%f", sep, va_arg(args, double));
+break;
+case 's':
 str = va_arg(args, char *);
 if (str == NULL)
-printf("(nil)");
-else
-printf("%s", str);
-
-if (i != n - 1 && separator != NULL)
-printf("%s", separator);
+str = "(nil)";
+printf("%s%s", sep, str);
+break;
+default:
+continue;
+}
+sep = ", ";
+}
 }
 
 printf("\n");
-
 va_end(args);
 }
