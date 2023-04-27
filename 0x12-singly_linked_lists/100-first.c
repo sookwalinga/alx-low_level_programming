45578a450c4b45a1831b9b696a22c89e430efbff
sam <sookwalinga@gmail.com>
2023-04-27 12:41:12 +0300

    fix: 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,24 +1,12 @@
 #include <stdio.h>
 
 /**
- * __attribute__ - Function to print a message before main
- * __constructor__ - This function is executed before main()
+ * premain - Function to print a message before main
  *
  * Return: Nothing
  */
-void __attribute__ ((constructor)) premain()
+void __attribute__ ((constructor)) premain(void)
 {
 printf("You're beat! and yet, you must allow,\n"
-       "I bore my house upon my back!\n");
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-return (0);
+   "I bore my house upon my back!\n");
 }
