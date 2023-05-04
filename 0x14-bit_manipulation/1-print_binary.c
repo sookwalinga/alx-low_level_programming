c5ab70a2dcf8eae52ab2efae78acb3af854fad89
sam <sookwalinga@gmail.com>
2023-05-05 02:16:22 +0300

    test: functions

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - Prints binary representation of number
+ * @n: number to print
+ */
+void print_binary(unsigned long int n)
+{
+if (n > 1)
+print_binary(n >> 1);
+
+putchar((n & 1) ? '1' : '0');
+}
